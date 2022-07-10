a552802e9cc5de0040a1e146f7c08030daff1a6a
yash goel <yashgoel236437@gmail.com>
2022-07-10 23:26:01 +0530

    tv movie problem

--- main.py
+++ main.py
@@ -20,9 +20,9 @@ if __name__ == "__main__":
         data_dict = json.load(f)["data_dict"]
     
     # remove empty values
-    data_list = [([i] + list(data_dict[i].values())[0:6]) for i in data_dict if type(data_dict[i]) == dict ]
+    data_list = [([i] + list(data_dict[i].values())) for i in data_dict if type(data_dict[i]) == dict ]
     data_list = [i for i in data_list if len(i)>1]
-    
+
     dataframe_structuring(data_list)
 
     print("Output generated and saved in temp")
